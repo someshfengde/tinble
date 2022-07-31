52d0b95211da2abb66f320ce2359a6125932cb9c
Somesh <42097653+someshfengde@users.noreply.github.com>
2022-07-31 18:41:15 +0000

    adding to the branch

---
+++ stationary/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Home from './Component/Home';
+import Login from './Component/Login';
+import Navbar from './Component/Navbar';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Login></Login>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ stationary/src/Component/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+function Home() {
+  return (
+    <div>
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="" />
+      <Card.Body>
+        <Card.Title>  Text Book</Card.Title>
+        <Card.Text>
+     22 
+        </Card.Text>
+        <Button variant="primary">Buy Now</Button>
+      </Card.Body>
+      
+    </Card>
+       <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="" />
+      <Card.Body>
+        <Card.Title>  Text Book</Card.Title>
+        <Card.Text>
+     22 
+        </Card.Text>
+        <Button variant="primary">Buy Now</Button>
+      </Card.Body>
+      
+    </Card>
+ 
+    </div>
+  )
+}
+
+export default Home
